23be5eb88b914a842ab6862ec76c61218bc27fd6
seanny3 <dev.seann@gmail.com>
2021-11-23 12:00:00 +0900

    JS : move block
    add Arrow Events using move_block function

--- main.js
+++ main.js
@@ -44,33 +44,46 @@ const BLOCKS = [
 			[[0,0],[1,0],[2,0],[3,0]],
 			[[0,0],[0,1],[0,2],[0,3]],
 			[[0,0],[1,0],[2,0],[3,0]],
-			[[0,0],[1,0],[2,0],[3,0]]
+			[[0,0],[0,1],[0,2],[0,3]]
 		]
 ];
-	
+// BLOCKS[shape][direction][space][upDown,side]
 
 document.body.onload = create_board();
-const random_block = Math.floor(Math.random()*7);
-const current_block = create_block(random_block);
+const current_block = create_block();		// create an undefiend direction ( return: BLOCK[shape][?] )
 
 // keyboard eventlistener
-let keydown_num = 0;
+let direction = 0;	// increase by 1 every time when you press SPACEBAR (0~3) --> defined direction
 document.addEventListener('keydown', (event) => {
 	const SPACEBAR = ' ';
 	if(event.key === SPACEBAR) {
-		delete_block(current_block, keydown_num);
-		keydown_num = ++keydown_num % 4
-		rotate_block(current_block, keydown_num);
+		delete_block(current_block[direction]);
+		direction = ++direction % 4;
+		rotate_block(current_block[direction]);
+	} 
+	else if (event.key === 'ArrowRight') {
+		delete_block(current_block[direction]);
+		move_block(current_block, direction, 0, 1);
+	} 
+	else if (event.key === 'ArrowLeft') {
+		delete_block(current_block[direction]);
+		move_block(current_block, direction, 0, -1);
+	} 
+	else if (event.key === 'ArrowDown') {
+		delete_block(current_block[direction]);
+		move_block(current_block, direction, 1, 0);
 	}
+	else {
+		return;
+	}
+				
 	
 })
 
 
 
 
-// function
-
-// import
+// functions
 
 // create a board
 function create_board() {
@@ -84,36 +97,47 @@ function create_board() {
 		}
 	}
 }
-
-
 // create a block
-function create_block(shape, direction = 0) {
-	const MAX_SPACES = 4;
-	for(let space = 0; space < MAX_SPACES; space++) {
-		const x = BLOCKS[shape][direction][space][0];
-		const y = BLOCKS[shape][direction][space][1];
-		board[x][y].style.setProperty('background-color', 'pink');
+function create_block() {
+	const block = BLOCKS[Math.floor(Math.random()*7)];
+	const MAX = 4;
+	for(let space = 0; space < MAX; space++) {
+		let upDown = block[0][space][0];
+		let side = block[0][space][1];
+		board[upDown][side].style.setProperty('background-color', 'pink');
 	}
-	return BLOCKS[shape];
+	return block;
 }
-
 // delete a existing block
-function delete_block(shape, direction) {
-	const MAX_SPACES = 4;
-	for(let space = 0; space < MAX_SPACES; space++) {
-		const x = shape[direction][space][0];
-		const y = shape[direction][space][1];
-		board[x][y].style.setProperty('background-color', 'white');
+function delete_block(block) {
+	const MAX = 4;
+	for(let space = 0; space < MAX; space++) {
+		let upDown = block[space][0];
+		let side = block[space][1];
+		board[upDown][side].style.setProperty('background-color', 'white');
 	}
 }
-
 // rotate a block
-function rotate_block(shape, direction) {
-	const MAX_SPACES = 4;
-	for(let space = 0; space < MAX_SPACES; space++) {
-		const x = shape[direction][space][0];
-		const y = shape[direction][space][1];
-		board[x][y].style.setProperty('background-color', 'pink');
+function rotate_block(block) {
+	const MAX = 4;
+	for(let space = 0; space < MAX; space++) {
+		let upDown = block[space][0];
+		let side = block[space][1];
+		board[upDown][side].style.setProperty('background-color', 'pink');
 	}
-	console.log(shape[direction]);
+}
+// move a block
+function move_block(block, current_direction, move_upDown, move_side) {
+	const MAX = 4;
+	for(let direction = 0; direction < MAX; direction++) {
+		for(let space = 0; space < MAX; space++) {
+			block[direction][space][0] += move_upDown;
+			block[direction][space][1] += move_side;
+		}
+	}
+	for(let space = 0; space < MAX; space++) {
+		let upDown = block[current_direction][space][0];
+		let side = block[current_direction][space][1];
+		board[upDown][side].style.setProperty('background-color', 'pink');
+	}
 }
